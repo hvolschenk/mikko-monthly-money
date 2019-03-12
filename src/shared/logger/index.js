cc448f3951d9c259db8b2b9888f8915b7071c5dc
Hendrik Volschenk <righteous.trespasser@gmail.com>
2019-03-13 00:02:50 +0200

    feat: add bunyan logger
    * add bunyan logger with custom minimal stream
    * remove chalk for formatting output

---
+++ src/shared/logger/index.js
@@ -0,0 +1,11 @@
+const bunyan = require('bunyan');
+
+const { application: { name }, log: { level, path } } = require('configuration');
+
+const minimalStream = require('./streams/minimal');
+
+module.exports = bunyan.createLogger({
+  name: name(),
+  serializers: { err: bunyan.stdSerializers.err },
+  streams: [minimalStream, { level: level(), path: path(name()) }],
+});
